699535cd388e9f3f3e1604f5ba066b1273c1f02e
Gabriel Santos <gabrielsantostroll@gmail.com>
2026-02-22 12:16:08 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,12 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-        <video width="320" height="240" controls>
-          <source src="nowrun.mp4" type="video/mp4">
-        </video>
       </main>
+      <video width="320" height="240" controls>
+        <source src="movie.mp4" type="video/mp4">
+        <source src="movie.ogg" type="video/ogg">
+        Your browser does not support the video tag.
+      </video>
 
       <Footer />
     </div>
